9d6ea312b53f74607aa4c94708f6f19892b237a1
PG_dk <programmer.dk85@gmail.com>
2024-07-09 13:19:45 +0530

    react app pushed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Everyone</h1>
+      <h2>Welcome to Learn how to Setup Jenkins for React Js Project</h2>
+       </div>
+  );
+}
+
+export default App;
