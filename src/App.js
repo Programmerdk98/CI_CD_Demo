f93465df6512e9b1149c6dc838597c7e13bc038c
PG_dk <programmer.dk85@gmail.com>
2024-07-09 14:06:09 +0530

    checks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
       <h1>Hello Everyone</h1>
       <h2>Welcome to Learn how to Setup Jenkins for React Js Project</h2>
-      <h3>Git check</h3>
+      <h3>Git checks</h3>
        </div>
   );
 }
